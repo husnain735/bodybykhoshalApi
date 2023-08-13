cd6ee7d375ef3545188a6b6ca101f61cbd7ad291
Husnain Ijaz <hijaz@informaticlabs.com>
2023-08-13 17:43:40 +0500

    Latest

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,4 @@
-﻿using bodybykhoshalApi.IService;
+using bodybykhoshalApi.IService;
 using bodybykhoshalApi.Models.Entities;
 using bodybykhoshalApi.Service;
 using Microsoft.AspNetCore.Authorization;
@@ -8,61 +8,70 @@ using static bodybykhoshalApi.Models.ViewModel.HttpRequest;
 
 namespace bodybykhoshalApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AdminController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AdminController : ControllerBase
+  {
+    private readonly IConfiguration _configuration;
+    private readonly IAdminService _adminService;
+    public AdminController(IConfiguration configuration, IAdminService adminService)
     {
-        private readonly IConfiguration _configuration;
-        private readonly IAdminService _adminService;
-        public AdminController(IConfiguration configuration, IAdminService adminService)
-        {
-            _configuration = configuration;
-            _adminService = adminService;
-        }
-        private Users getClaims()
-        {
-            Users usr = new Users();
-            ClaimsPrincipal user = User;
-            if (user.Identity.IsAuthenticated)
-            {
-                Claim UserGuidClaim = user.FindFirst(ClaimTypes.NameIdentifier);
-                string UserGuid = UserGuidClaim?.Value;
-                usr.UserGUID = UserGuid;
-                return usr;
-            }
-            return usr;
-        }
-        [Authorize]
-        [HttpGet("GetAllCustomers")]
-        public IActionResult GetAllCustomers()
-        {
-            var packages = _adminService.GetAllCustomers();
-            return Ok(packages);
-        }
-        [Authorize]
-        [HttpPost("GetAdminChatWithCustomer")]
-        public IActionResult GetAdminChatWithCustomer(GetAdminChatWithCustomerRequestHandler request)
-        {
-            var userClaim = getClaims();
-            request.SenderOne = userClaim.UserGUID;
-            var packages = _adminService.GetAdminChatWithCustomer(request);
-            return Ok(packages);
-        }
-        [Authorize]
-        [HttpPost("saveChatForAdmin")]
-        public IActionResult saveChatForAdmin(SaveChatRequestHandler request)
-        {
-            var userClaim = getClaims();
-            request.SenderOne = userClaim.UserGUID;
-            var user = _adminService.saveChatForAdmin(request);
-            return Ok(new { Success = user });
-        }
-        [Authorize]
-        [HttpPost("readAllMessages")]
-        public IActionResult readAllMessages(GetAdminChatWithCustomerRequestHandler request)
-        {
-            var user = _adminService.readAllMessages(request);
-            return Ok(new { Success = user });
-        }
+      _configuration = configuration;
+      _adminService = adminService;
     }
+    private Users getClaims()
+    {
+      Users usr = new Users();
+      ClaimsPrincipal user = User;
+      if (user.Identity.IsAuthenticated)
+      {
+        Claim UserGuidClaim = user.FindFirst(ClaimTypes.NameIdentifier);
+        string UserGuid = UserGuidClaim?.Value;
+        usr.UserGUID = UserGuid;
+        return usr;
+      }
+      return usr;
+    }
+    [Authorize]
+    [HttpGet("GetAllCustomers")]
+    public IActionResult GetAllCustomers()
+    {
+      var packages = _adminService.GetAllCustomers();
+      return Ok(packages);
+    }
+    [Authorize]
+    [HttpPost("GetAdminChatWithCustomer")]
+    public IActionResult GetAdminChatWithCustomer(GetAdminChatWithCustomerRequestHandler request)
+    {
+      var userClaim = getClaims();
+      request.SenderOne = userClaim.UserGUID;
+      var packages = _adminService.GetAdminChatWithCustomer(request);
+      return Ok(packages);
+    }
+    [Authorize]
+    [HttpPost("saveChatForAdmin")]
+    public IActionResult saveChatForAdmin(SaveChatRequestHandler request)
+    {
+      var userClaim = getClaims();
+      request.SenderOne = userClaim.UserGUID;
+      var user = _adminService.saveChatForAdmin(request);
+      return Ok(new { Success = user });
+    }
+    [Authorize]
+    [HttpPost("readAllMessages")]
+    public IActionResult readAllMessages(GetAdminChatWithCustomerRequestHandler request)
+    {
+      var user = _adminService.readAllMessages(request);
+      return Ok(new { Success = user });
+    }
+    [Authorize]
+    [HttpGet("getCustomersBookings")]
+    public IActionResult getCustomersBookings()
+    {
+      var userClaim = getClaims();
+      var UserGUID = userClaim.UserGUID;
+      var user = _adminService.getCustomersBookings(UserGUID);
+      return Ok(user);
+    }
+  }
 }
